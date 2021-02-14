cde161c378b98acfa3459f9ba6291b8b5f9f2ba0
KolodziejczykTomasz <kolodziejczyk.tomasz44@gmail.com>
2021-02-14 05:50:32 +0100

    Added breakeSection component.

--- src/components/breakeSection.js
+++ src/components/breakeSection.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React from "react"
 
-const BreakeSection = ()=>(
-    <>
-    <div>
-        
-    </div>
-    </>
-);
-export default BreakeSection;
+import styled from "styled-components"
+
+const WrapperStartBreakeLine = styled.div`
+  display: grid;
+  margin: 40px 0 50px 0;
+  height: 80px;
+`
+
+const WrapperItemLeft = styled.div`
+  grid-column-start: 1;
+  grid-column-end: 6;
+  margin: 5px 0;
+`
+
+const WrapperItemCentral = styled.div`
+  grid-column-start: 6;
+  grid-column-end: 9;
+`
+const WrapperItemRight = styled.div`
+  grid-column-start: 9;
+  grid-column-end: 15;
+  margin: 5px 0;
+`
+
+const WrapperText = styled.div`
+  text-align: center;
+  font-size: 30px;
+  font-weight: 600;
+  color: #5b5b5b;
+  letter-spacing: 0.2rem;
+`
+
+const BreakeSection = ({ children }) => (
+  <WrapperStartBreakeLine>
+    <WrapperItemLeft id="wrapperItemLeft">
+      <hr style={{ borderTop: "1.5px solid #1680BC" }} />
+    </WrapperItemLeft>
+    <WrapperItemCentral id="wrapperItemCentral">
+      <WrapperText id="wrapperText">{children}</WrapperText>
+    </WrapperItemCentral>
+    <WrapperItemRight id="wrapperItemRight">
+      <hr style={{ borderTop: "1.5px solid #1680BC" }} />
+    </WrapperItemRight>
+  </WrapperStartBreakeLine>
+)
+export default BreakeSection
