b51a1ac5d26f2d94ca0d838966225bffd3507d12
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2021-02-22 21:47:49 +0100

    Added new post data.

---
+++ src/data/news.js
@@ -0,0 +1,224 @@
+import foto1 from '../assets/images/news/walentyki2021.png'
+import foto2 from "../assets/images/news/spiewamy_2021.jpg"
+import foto3 from "../assets/images/news/angielski.jpg"
+
+export const news = [
+  {
+    id: 1,
+    data: "14.02.2021",
+    title: "Walentynki 2021",
+    excerpt: "Wyjątkowy koncert Walentynkowy online",
+    content: [
+      {
+        id: 1,
+        p1: "",
+      },
+      {
+        id: 2,
+        p2: "",
+      },
+      {
+        id: 3,
+        p3: "",
+      },
+      {
+        id: 4,
+        p4: "",
+      },
+      {
+        id: 5,
+        p5: "",
+      },
+      {
+        id: 6,
+        p6: "",
+      },
+      {
+        id: 7,
+        p7: "",
+      },
+      {
+        id: 8,
+        p8: "",
+      },
+      {
+        id: 9,
+        p9: "",
+      },
+      {
+        id: 10,
+        p10: "",
+      },
+    ],
+    url: { foto1 },
+    link: "",
+    gallery: [
+      {
+        id: 1,
+        url: "",
+      },
+      {
+        id: 2,
+        url: "",
+      },
+      {
+        id: 3,
+        url: "",
+      },
+      {
+        id: 4,
+        url: "",
+      },
+      {
+        id: 5,
+        url: "",
+      },
+    ],
+  },
+  {
+    id: 2,
+    data: "2.11.2020",
+    title: "Przegląd Piosenki i Pieśni Patriotycznej.",
+    excerpt: "Gminny Ośrodek Kultury i Sportu w Pilnku zaprasza do udziału w konkursie pt. 'Śpiewamy dla Ojczyzny' Przegląd Piosenki i Pieśni Patriotycznej ",
+    content: [
+      {
+        id: 1,
+        p1: "",
+      },
+      {
+        id: 2,
+        p2: "",
+      },
+      {
+        id: 3,
+        p3: "",
+      },
+      {
+        id: 4,
+        p4: "",
+      },
+      {
+        id: 5,
+        p5: "",
+      },
+      {
+        id: 6,
+        p6: "",
+      },
+      {
+        id: 7,
+        p7: "",
+      },
+      {
+        id: 8,
+        p8: "",
+      },
+      {
+        id: 9,
+        p9: "",
+      },
+      {
+        id: 10,
+        p10: "",
+      },
+    ],
+    url: { foto2 },
+    link: "",
+    gallery: [
+      {
+        id: 1,
+        url: "",
+      },
+      {
+        id: 2,
+        url: "",
+      },
+      {
+        id: 3,
+        url: "",
+      },
+      {
+        id: 4,
+        url: "",
+      },
+      {
+        id: 5,
+        url: "",
+      },
+    ],
+  },
+  {
+    id: 3,
+    data: "3.07.2020",
+    title: "Kurs języka angielskiego i kurs komputerowy",
+    excerpt:
+      "Gminny Ośrodek Kultury i Sportu w Pilniku serdecznie zaprasza na kurs języka angielskiego i kurs komputerowy. Zajęcia odbywać się będą w GOKiS w Pilniku. ",
+    content: [
+      {
+        id: 1,
+        p1:
+          "Rozpoczęcie kursu oraz terminy ustalone zostaną po zebraniu odpowiedniej ilości zgłoszeń (grupy 12-osobowe).",
+      },
+      {
+        id: 2,
+        p2: "",
+      },
+      {
+        id: 3,
+        p3: "",
+      },
+      {
+        id: 4,
+        p4: "",
+      },
+      {
+        id: 5,
+        p5: "",
+      },
+      {
+        id: 6,
+        p6: "",
+      },
+      {
+        id: 7,
+        p7: "",
+      },
+      {
+        id: 8,
+        p8: "",
+      },
+      {
+        id: 9,
+        p9: "",
+      },
+      {
+        id: 10,
+        p10: "",
+      },
+    ],
+    url: { foto3 },
+    link: "",
+    gallery: [
+      {
+        id: 1,
+        url: "",
+      },
+      {
+        id: 2,
+        url: "",
+      },
+      {
+        id: 3,
+        url: "",
+      },
+      {
+        id: 4,
+        url: "",
+      },
+      {
+        id: 5,
+        url: "",
+      },
+    ],
+  },
+]
